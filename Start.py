54f1b770b0153fdb15f5e19cb4a6eb991b89bf98
Darsiy <57631796+Darsiy@users.noreply.github.com>
2020-01-13 17:02:10 +0500

    Update Start.py

--- Start.py
+++ Start.py
@@ -1,16 +1,79 @@
 import random
+def magic_start(self, igrok, monstr, level):
+    if level:
+        monstr.hp -= level*2+igrok.maguron
+    else:
+        g = random.randint(0, 2)
+        monstr.hp -= g*igrok.maguron
+        if g != 1:
+            igrok.hp -= igrok.maguron//2
+def magic_energy_ball(self, igrok, monstr, level):
+    if level:
+        monstr.hp -= igrok.maguron*(2 + level / 10)
+    else:
+        g = random.randint(0, 2)
+        monstr.hp -= g*igrok.maguron
+        if g != 1:
+            igrok.hp -= igrok.maguron//2
+def magic_energy_wave(self, igrok, monstr, level):
+    if level:
+        monstr.sposobnosti += 1
+    else:
+        g = random.randint(0, 2)
+        monstr.sposobnosti += g
+        if g != 1:
+            igrok.sposobnosti += 1
+def magic_wall(self, igrok, monstr, level):
+    if level:
+        igrok.hp += level * 20
+    else:
+        igrok.sposobnosti += 1
+def sword_start(self, igrok, monstr, level):
+    if level:
+        monstr.hp -= level*2+igrok.oburon
+    else:
+        g = random.randint(0, 2)
+        monstr.hp -= g*igrok.oburon
+        if g != 1:
+            igrok.hp -= igrok.oburon // 2
+def sword_fast_attack(self, igrok, monstr, level):
+    if level:
+        g = random.randint(0, 2)
+        monstr.hp -= igrok.oburon
+        if g:
+            monstr.krovotechenie += level
+    else:
+        pass
+def sword_slow_attack(self, igrok, monstr, level):
+    if level:
+        g = random.randint(0, 2)
+        monstr.hp -= igrok.oburon*level
+        if g:
+            monstr.sposobnosti += 1
+    else:
+        pass
+def sword_normal_attack(self, igrok, monstr, level):
+    if level:
+        g = random.randint(0, 2)
+        monstr.hp -= igrok.oburon*level
+        if g:
+            igrok.hp += level * 40
+    else:
+        pass
 class Navk:
     def __init__(self, func, level):
         self.level = level
         self.func = func
 
-    def deistvne(self, igrok, monstr):
+    def deistvie(self, igrok, monstr):
         self.func(igrok, monstr, self.level)
 
     def level_up(self, g):
         self.level += g
 class Persona:
     def __init__(self, textura, drevo, uroven, supersoundpack):
+        self.krovotechenie = 0
+        self.sposobnosti = 0
         self.textura = textura
         self.drevo = drevo.drevo
         self.uroven = uroven
@@ -25,7 +88,7 @@ class Persona:
         self.maguron = (self.intelect + self.shizn) * 10
         self.hsuperpoint = uroven# очки характеристик
         self.megapoint = uroven // 5# очки навыков
-        self.pd = uroven + self.sila + self.intelekt
+        self.pd = level
         self.inventar = []
         self.snarasheinie = {'shlem': None, 'kirasa' : None, 'posoh_or_mech': None} # снаряжение персонажа(None значит нету)
 
@@ -52,6 +115,10 @@ class Persona:
             self.hp = self.shizn * 10
         else:
             self.inventar.append(self.snarashenie[t])
+            self.sila -= self.snarashenie[t].haracteristik[-1]
+            self.lovkost -= self.snarashenie[t].haracteristik[1]
+            self.intelekt -= self.snarashenie[t].haracteristik[0]
+            self.shizn -= self.snarashenie[t].haracteristik[2]
             self.snarashenie[t] = snaraga
             self.sila += snaraga.haracteristik[-1]
             self.lovkost += snaraga.haracteristik[1]
@@ -61,27 +128,62 @@ class Persona:
         del self.inventar[self.inventar.index(snaraga)]
 class Monstr:
     def __init__(self, textura, level, soundpack):
+        self.sposobnosti = 0
+        self.krovotechenie = 0
         self.textura = textura
         self.napravlenie = 'sila' if level <= 3 else (random.choice(['sila', 'fire', 'water', 'sila', 'fire', 'dark']))
-        self.dxp = level * 3**(['sila', 'fire', 'water', 'dark'].index(self.napravlenie))
+        self.dxp = level * (['sila', 'fire', 'water', 'dark'].index(self.napravlenie)+1)
         self.level = level
         self.uron = 2**level
         self.hp = 2**level * random.randint(1, 5)
+class Boss:
+    def __init__(self, textura, level, soundpack, igrok):
+        self.sposobnosti = 0
+        self.krovotechenie = 0
+        self.textura = textura
+        self.napravlenie = 'sila' if level <= 3 else (random.choice(['sila', 'fire', 'water', 'sila', 'fire', 'water', 'dark']))
+        self.dxp = level * (['sila', 'fire', 'water', 'dark'].index(self.napravlenie)+1) * (1 if self.napravlenie != 'dark' else 3)
+        self.level = level
+        self.uron = 2**level
+        self.hp = 2**level * random.randint(1, 5)
+        if self.napravlenie == 'sila':
+            self.hp *= 3
+        if self.napravlenie == 'fire':
+            self.uron *= 3
+        if self.napravlenie == 'water':
+            self.uron *= 3
+            self.hp *= 3
+        if self.napravlenie == 'dark':
+            self.level += igrok.level
+            self.uron = 2**level
+            self.hp *= 2**igrok.level
 class Bitva_c_monstr:
     def __init__(self, igrok, monstr):
         vd = igrok.getnav()
         kd = igrok.pd
         while monstr.hp != 0 and igrok.hp != 0:
-            for i in range(kd):
-                a = 0#не ноль а запрпос ноиера навыка из предварительно показанного игроку списка, соответствующего списку выдаваемому igrok.getnav()
-                vd[a - 1](monstr)
-                if monstr.hp > 0:
-                    pass
-                else:
-                    igrok.monstr_ubit(monstr)
-                    igrok.hp = igrok.shizn*10
-                    return 1
-            igrok.attack(monstr)
+            if igrok.sposobnosti != 0:
+                for i in range(3):
+                    a = 0#не ноль а запрпос номера навыка из предварительно показанного игроку списка, соответствующего списку выдаваемому igrok.getnav()
+                    vd[a - 1].deistvie(monstr)
+                    if monstr.hp > 0:
+                        pass
+                    else:
+                        igrok.monstr_ubit(monstr)
+                        igrok.hp = igrok.shizn*10
+                        return 1
+            else:
+                igrok.sposobnosti -= 1
+            if monstr.krovotechenie:
+                monstr.hp *= (1 - monstr.krovotechenie/10)
+                monstr.krovotechenie = 0
+            if igrok.krovotechenie:
+                igrok.hp *= (1 - igrok.krovotechenie/10)
+                igrok.krovotechenie = 0
+            if monstr.sposobnosti != 0:
+                igrok.attack(monstr)
+            else:
+                monstr.sposobnosti -= 1
             if igrok.hp <= 0:
                 igrok.hp = igrok.shizn*10
                 return 1#тут завершение боя, игрок не может умереть, если хп в ноль - битва завершается, игрок уходит, готовится, и возвращается
@@ -99,32 +201,23 @@ class Snaraga:
         self.haracteristik = [i, l, sh, s]
 class Drevo:
     def __init__(self, specialsoundpack):
-        self.drevo = {'magic': [0, {'energy ball': [0, {'fireball': [0],
-                                                        'waterball': [0],
-                                                        'lightning ball': [0],
-                                                        }],
-                                'energy wall': [0, {'firewall': [0],
-                                                    'waterwall': [0],
-                                                    'lightning wall': [0],
-                                                    }],
-                                'energy wave': [0, {'fire wave': [0],
-                                                    'water wave': [0],
-                                                    'lightning': [0],
-                                                    }],
-                                'positive charge': [0, {'treatment': [0],
-                                                        'beneficial effect': [0],
-                                                        'call of God': [0],},
-                                                    ]},
-                                ],
-                      'sword': [0, {'fast attack': [0, {'the cycle of swords': [0]}],
-                                'slow attack': [0, {'a fatal puncture': [0],
-                                                    }],
-                                'normal attack': [0, {'blood to the God of swords': [0],
-                                                      }],
-                                'counterattack': [0],
-                                                    },
-                                    ]}
-#соундпаки - списки с музыкой, прогигрываемой при определённых ситуациях. soundpack = [получение урона, нанесение урона]\
-#        supersoundpack = [получение урона, нанесение урона, лечение, шаги, переход на новый уровень] specialsoundpack = [поднятие уровня навыка, изучение навыка]
-#картинки снаряги - тип('shlem' и т.д), '_', уровень, '.jpg'
-        
+        self.drevo = {'magic': [Navk(magic_start, 0), {'energy ball': [Navk(magic_energy_ball, 0)],
+                                'energy wall': [Navk(magic_energy_wall, 0)],
+                                'energy wave': [Navk(magic_energy_wave, 0)]}],
+                      'sword': [Navk(sword_start, 0), {'fast attack': [Navk(sword_fast_attack, 0)],
+                                'slow attack': [Navk(sword_slow_attack, 0)],
+                                'normal attack': [Navk(sword_normal_attack, 0)]}]}
+class Karta:
+    def __init__(self, x, y, level, soundpackmonstr, igrok, soundpackboss):
+        self.karta = [[None if random.randint(0, 5) != 0 else (Monstr('Monstr' + str(level) + '.jpg', level, soundpackmonstr) if random.randint(0, 10) != 0 else Boss('Boss' + str(level) + '.jpg', level, soundpackboss, igrok)) for i in range(x)] for i in range(y)]
+        self.karta[y//2][x//2] = igrok
+    def check(self):
+        for i in self.karta:
+            for j in i:
+                if type(j) in (type(Monstr(1, 1, 1)), type(Boss(1, 1, 1, 1))):
+                    return False
+        return True
+#соундпаки - списки с музыкой, прогигрываемой при определённых ситуациях. soundpack = [получение урона, нанесение урона, стан]\
+#        supersoundpack = [получение урона, нанесение урона, стан, шаги, переход на новый уровень] specialsoundpack = [поднятие уровня навыка, изучение навыка]
+#названия картинки снаряги - тип('shlem' и т.д) + '_' + уровень + '.jpg'
+#sposobnosti - уровень стана, сколько ходов пропустит self
